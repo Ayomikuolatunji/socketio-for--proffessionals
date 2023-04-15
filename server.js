0333923e74ff470d1c864dbbc9d9f1c318e1fcf9
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-04-15 16:54:52 +0100

    socket server setup

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
 const publicPath = path.join(__dirname, "public");
-
+const socketIO = require("socket.io");
 const app = express();
 
 app.use(express.static(publicPath));
+
+let server = http.createServer(app);
+
+let io = socketIO(server);
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
